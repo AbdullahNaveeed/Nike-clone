4105a72c3ae0af70ea29f639de47f2f6c8110ee5
Abdullah Naveed <abdullahnaveeed01@gmail.com>
2024-10-07 12:43:24 +0500

    Hello

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col min-h-screen max-container justify-center gap-10 "
+    ></section>
+  );
+};
+
+export default Hero;
