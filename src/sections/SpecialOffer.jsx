4e1c673546038801279f3ffa4ffb040df7803dba
Abdullah Naveed <abdullahnaveeed01@gmail.com>
2024-10-08 11:21:15 +0500

    Making Special Offer Section

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className='flex justify-wrap item-center max-xl:flex-col-reverse gap-10 max-container'></section>
   )
 }
 
