67d7a3d6b037dfb4a2d8319cd68af0f4f82739f6
Abdullah Naveed <abdullahnaveeed01@gmail.com>
2024-10-08 10:55:59 +0500

    Make Services file and update components

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import {services } from "../constants";
 const Services = () => {
   return (
-    <section className=''> 
+    <section className='max-container flex justify-center flex-wrap gap-9'> 
+
 
     </section>
   )
